d3f7dbea40b46fc0f9d386260f4af637a454cf3a
Israel7E4 <israel.ferron.7E4@itb.cat>
2020-11-04 10:37:48 +0100

    Practica

---
+++ practicaif.java
@@ -0,0 +1,10 @@
+public class practicaif {
+        public static void main(String[] args){
+                int n = 3;
+                if (n < 5)
+                        System.out.println("Mes petit que 5");
+                else
+                        System.out.println("Mes gran que 5");
+        }
+}
+
